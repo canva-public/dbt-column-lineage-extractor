2e245b2ef354a031ac2165ada339f3a94ae3df77
Wen Wu <wenwu@canva.com>
2024-10-15 13:58:12 +1100

    Update version to 0.1.1 in setup.py

--- py_package/setup.py
+++ py_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dbt_column_lineage_extractor',
-    version='0.1.0',
+    version='0.1.1',
     description='A package for extracting dbt column lineage',
     long_description=open('../README.md').read(),
     long_description_content_type='text/markdown',
